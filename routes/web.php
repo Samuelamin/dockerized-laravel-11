2985ae773b5e2ba09777392d6e3c7674ea5df47e
samuelamin <samuelamin1999@gmai.com>
2024-11-27 08:23:58 +0200

    test docker hup

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return 'test docker hup';
+});
+
 Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+    'auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
